ec4f4221c6b90e75a10659b518012cd729894e63
amatur <Sherlock@Amaturs-MacBook-Pro.local>
2018-12-01 23:03:08 -0500

    cxx3

--- lu.cpp
+++ lu.cpp
@@ -20,27 +20,27 @@ void swapRows(double* mat, int row1, int row2){
 
 
 void LUSolve(int dim, double** A2, double* B, double *x, double eps=1e-10){
-	  
-	 /* make a copy of A2 matrix, as the algo changes A2 in place*/ 
+
+	 /* make a copy of A2 matrix, as the algo changes A2 in place*/
 	double **A = (double **)calloc( dim, sizeof(double *));
-	
+
 	for(int i = 0; i < dim; i++ )
 	{
 		A[i] = (double *)calloc( dim, sizeof(double));
 	}
-	
+
 	for(int i = 0; i<dim; i++){
 		for(int j = 0; j<dim; j++){
 			A[i][j] = A2[i][j];
 		}
 	}
-	
-	
+
+
 	/* Initialization of the pivot column */
 	for(int k = 0; k < dim; k++){
 		/* Find the k-th pivot: */
 		int i_max = k;
-		
+
 		// find the largest element of this column, iterate through all row
 		for(int i = k; i<dim; i++){
 			if(fabs(A[i][k]) > fabs(A[i_max][k])){
@@ -48,48 +48,48 @@ void LUSolve(int dim, double** A2, double* B, double *x, double eps=1e-10){
 			}
 		}
 		if (fabs(A[i_max][k]) < eps) return; //fail
-	
-	
+
+
 		if (fabs(A[i_max][k]) != k){
 			swapRows(A, k, i_max);
 			swapRows(B, k, i_max);
-			
+
 		}
 		for (int i = k + 1; i<dim; i++){
 			double f = A[i][k] / A[k][k];
 			A[i][k] = f;
-			
-			
+
+
 			for (int j = k + 1; j < dim ; j++){
 				A[i][j] = A[i][j] - A[k][j] * f;
 			}
 		}
-		
-		
+
+
 	}
 
 
 	for (int i = 0; i < dim; i++) {
 		x[i] = B[i];
-		
+
 		for (int k = 0; k < i; k++){
 			x[i] -= A[i][k] * x[k];
 		}
-			
+
 	}
-	
+
 	//Ux = y
 	for (int i = dim - 1; i >= 0; i--) {
 		for (int k = i + 1; k < dim; k++){
 			x[i] -= A[i][k] * x[k];
 		}
-		
+
 		x[i] = x[i] / A[i][i];
 	}
-	
+
 }
 
-/*
+
 int main(){
 	int n = 4;
 	double A2[4][4] = {
@@ -98,28 +98,28 @@ int main(){
                         {2., -1., 10., -1.},
                         {0.0, 3., -1., 8.}
                        };
-           
+
     double b2[4] =  {6., 25., -11., 15.};
     double expected_x[4] = {1.0,  2.0, -1.0, 1.0};
-    
-        
+
+
     double **A ;
     double	*b, *x;
-    
+
     init2d(&A, n);
     init1d(&b, n);
     init1d(&x, n);
-    
+
     for(int i = 0; i<n; i++){
 		for(int j = 0; j<n; j++){
 			A[i][j] = A2[i][j];
 		}
 	}
-    
+
     for(int j = 0; j<n; j++){
 		b[j] = b2[j];
 	}
-    
+
     print(A, n);
     print(b, n);
 
@@ -129,4 +129,3 @@ int main(){
 
 	return 0;
 }
-*/
