403b303dc5568d21cdda57b650123f70a100a3ce
amatur <Sherlock@Amaturs-MacBook-Pro.local>
2018-12-06 22:51:04 -0500

    cu

--- jacobi_cublas.cu
+++ jacobi_cublas.cu
@@ -37,7 +37,7 @@ float getError(float *x, float *xnew, int N)
 
   cublasSaxpy(N, -1.0f, d_x, 1, d_y, 1);
 
-  cublasGetVector(N, sizeof(yy[0]), d_y, 1, y, 1);
+  cublasGetVector(N, sizeof(yy[0]), d_y, 1, yy, 1);
   float sum = cublasSasum (N,  d_y, 1);
   cublasShutdown();
   return sum;
